170c3d01110abad5bc60cc98a2f476eedb3ee1fd
Gabriel Aubut-Lussier <dalzhim@hotmail.com>
2016-02-28 19:23:27 -0500

    Création d'un terrain et d'un début de véhicule.

---
+++ Assets/Mouvement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouvement : MonoBehaviour {
+	public GameObject chemin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		this.transform.Translate(new Vector3(.01f, 0f, 0f) * Time.deltaTime);
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (6f, 0f), ForceMode2D.Force);
+	}
+}
