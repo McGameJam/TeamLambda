0025d790567decfdcc3e95b31668f2f1b43780d2
Gabriel Aubut-Lussier <dalzhim@hotmail.com>
2016-03-20 01:14:18 -0400

    Merge d'un conflit.

--- Assets/Scripts/UnitAction.cs
+++ Assets/Scripts/UnitAction.cs
@@ -4,11 +4,8 @@ using System.Collections;
 public abstract class UnitAction : MonoBehaviour {
 	public string nom;
 	public bool active = true;
-<<<<<<< 4e1f7cdbee8291c2b506917af46cf5d73673fc34
 	public Sprite icon;
-=======
 	public bool oncePerUnit = false;
->>>>>>> Élimination de classes non nécessaires et création de Prefabs pour configurer le jeu.
 
 	public virtual void Action ()
 	{
