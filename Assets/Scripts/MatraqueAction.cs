d39cf404bbb8498f70393ecc70457c68dc12151e
Gabriel Aubut-Lussier <dalzhim@hotmail.com>
2016-03-20 13:44:09 -0400

    Ajout d'une action pour matraquer et ajout d'un Win Screen.

---
+++ Assets/Scripts/MatraqueAction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MatraqueAction : UnitAction {
+
+	public int targetMinRange = 0;
+	public int targetMaxRange = 1;
+	public CellGridStateTarget.eTargetType targetType = CellGridStateTarget.eTargetType.Point;
+
+	public override void Action ()
+	{
+		GameObject cellGridObj = GameObject.Find ("CellGrid");
+		CellGrid grid = cellGridObj.GetComponent<CellGrid> ();
+		if (grid != null && grid.CellGridState is CellGridStateUnitSelected) {
+			Unit unit = (grid.CellGridState as CellGridStateUnitSelected).selection;
+			Predicate<Unit> predicate = (u) => {
+				var hasActionPoints = unit.ActionPoints > 0;
+				var isDifferent = unit != u;
+				var isEnemy = unit.PlayerNumber != u.PlayerNumber;
+				var distance = unit.Cell.GetDistance (u.Cell);
+				var isFarEnough = distance >= this.targetMinRange; 
+				var isCloseEnough = distance <= this.targetMaxRange;
+				var isWayClearOfObstacles = !unit.IsObstacleInTheWay (u, unit.Cell);
+				return hasActionPoints && isDifferent && isEnemy && isCloseEnough && isFarEnough && isWayClearOfObstacles;
+			};
+			Action<Unit> unitAction = (Unit target) => {
+				Animator animator = unit.GetComponent<Animator>();
+				animator.SetTrigger("Matraque");
+				unit.DealDamage(target);
+				base.Action ();
+			};
+			grid.CellGridState = new CellGridStateTarget (grid, unit, this.targetType, predicate, unitAction);
+		}
+	}
+}
--- Assets/TBS Framework/Scripts/Core/CellGridStateGameOver.cs
+++ Assets/TBS Framework/Scripts/Core/CellGridStateGameOver.cs
@@ -1,3 +1,7 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
 public class CellGridStateGameOver : CellGridState
 {
     public CellGridStateGameOver(CellGrid cellGrid) : base(cellGrid)
@@ -6,5 +10,14 @@ public class CellGridStateGameOver : CellGridState
 
     public override void OnStateEnter()
     {
+		GameObject.Find ("Main Camera").SetActive (false);
+		GameObject.Find ("GUICamera").SetActive (false);
+		GameObject obj = GameObject.Find ("WinButton");
+		var gagnant = _cellGrid.Units.Select (u => u.PlayerNumber).Distinct ().ToList ();
+		if (gagnant.Count == 1 && gagnant.ElementAt (0) == 0) {
+			GameObject.Find ("VictoirePoliciers").SetActive (false);
+		} else {
+			GameObject.Find ("VictoireManifestants").SetActive (false);
+		}
     }
 }
